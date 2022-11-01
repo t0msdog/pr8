0842e1021ebba2c09fb4ce6ce1cc5642b2edbe59
t0msdog <115209934+Rabbit546@users.noreply.github.com>
2022-11-01 10:19:54 +0800

    Initial commit

---
+++ pr8/android/app/src/main/kotlin/com/example/part_8/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.part_8
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ pr8/android/app/src/main/kotlin/com/example/pr8/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pr8
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
